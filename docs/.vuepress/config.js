1fbdbe706a480e7ca89e70e7c0bfc207eecc25a8
Ostask <502724802@qq.com>
2021-02-10 21:53:07 +0800

    推送一下东西

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,13 @@
+const sidebar = require("./utils/getSideBar");
+
 module.exports = {
     base:'/azis-blog/',
     title: 'Welcome to Azis home',
-    description: 'Just playing with me'
+    description: 'Just playing with me',
+    themeConfig: {
+        nav: [
+            { text: '主页', link: '/' },
+        ],
+        sidebar:sidebar
+    }
 }
